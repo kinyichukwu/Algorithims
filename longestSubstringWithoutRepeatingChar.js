8d51d9659ea561e202de531b41546f6c54a82e56
Kinyichukwu Oselukwue <81447346+kinyichukwu@users.noreply.github.com>
2022-07-04 14:10:50 +0100

    Update longestSubstringWithoutRepeatingChar.js

--- longestSubstringWithoutRepeatingChar.js
+++ longestSubstringWithoutRepeatingChar.js
@@ -1,1 +1,25 @@
-
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    const pn = []
+    if (x < 0) {
+        x = x * -1
+        pn[0] = -1
+    }else{ pn[0] = 1}
+    console.log(pn[0]);
+  let str = '' + x;
+  let reverse = '';
+  for (i = 0; i < str.length; i++) {
+    reverse = str[i] + reverse;
+  }
+  reverseNumber = reverse - 0;
+  reverseNumber = reverseNumber * pn[0]
+  console.log(reverse);
+  if (x === reverseNumber) {
+    return true;
+  } else {
+    return false;
+  }
+};
